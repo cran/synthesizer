3b02c9f7f0b232d15c61d5bf83ed48c748cc5c7a
Mark van der Loo <mark.vanderloo@gmail.com>
2024-06-13 17:40:01 +0000

    version 0.2.0

---
+++ inst/doc/introduction.R
@@ -0,0 +1,27 @@
+# load_package
+library(synthesizer)
+# check the package version
+packageVersion("synthesizer")
+
+# load_chickweight
+data(iris)
+head(iris)
+
+# synthesize_iris
+set.seed(1)
+synth_iris <- synthesize(iris)
+
+# plot
+par(mfrow=c(1,2))
+plot(Sepal.Length ~ Petal.Length, data=iris, col=iris$Species, pch=16,las=1,xlim=c(0,7),ylim=c(4,8),main="Original")
+legend("topleft",legend=levels(iris$Species),col=1:3,pch=16,bty="n")
+plot(Sepal.Length ~ Petal.Length, data=synth_iris, col=iris$Species,pch=16,las=1,xlim=c(0,7),ylim=c(4,8),main="Synthesized")
+legend("topleft",legend=levels(iris$Species),col=1:3,pch=16,bty="n")
+
+# synthesize_more
+more_synth <- synthesize(iris, n=250)
+dim(more_synth)
+
+# <unlabeled code block>
+pmse(synth=synth_iris, real=iris)
+
